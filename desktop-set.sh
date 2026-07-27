83e85397c3c3689849c2a4a58c27e2e49c7f3944
JavierC <javierc@noreply.com>
2026-07-27 12:24:57 -0400

    limit:::script::to::one:::arg

--- desktop-set.sh
+++ desktop-set.sh
@@ -2,46 +2,58 @@
 . ./base-neko-pkgs.sh
 
 xfce(){
-cp -rfv xfce/etc /
-cp -rfv xfce/usr /
-xbps-install -Sy $PACKAGES_XFCE
+    cp -rfv xfce/etc /
+    cp -rfv xfce/usr /
+    xbps-install -Sy $PACKAGES_XFCE
 }
+
 niri(){
-cp -rfv niri/etc /
-cp -rfv niri/usr /
-xbps-install -Sy $PACKAGES_NIRI
+    cp -rfv niri/etc /
+    cp -rfv niri/usr /
+    xbps-install -Sy $PACKAGES_NIRI
 }
+
 kde(){
-cp -rfv kde/etc /
-cp -rfv kde/usr /
-xbps-install -Sy $PACKAGES_KDE
+    cp -rfv kde/etc /
+    cp -rfv kde/usr /
+    xbps-install -Sy $PACKAGES_KDE
 }
+
 mate(){
-cp -rfv mate/etc /
-cp -rfv mate/usr /
-xbps-install -Sy $PACKAGES_MATE
+    cp -rfv mate/etc /
+    cp -rfv mate/usr /
+    xbps-install -Sy $PACKAGES_MATE
 }
+
 labwc(){
-cp -rfv labwc/etc /
-cp -rfv labwc/usr /
-xbps-install -Sy $PACKAGES_LABWC
+    cp -rfv labwc/etc /
+    cp -rfv labwc/usr /
+    xbps-install -Sy $PACKAGES_LABWC
 }
+
 lxqt(){
-cp -rfv lxqt/etc /
-cp -rfv lxqt/usr /
-xbps-install -Sy $PACKAGES_LXQT
+    cp -rfv lxqt/etc /
+    cp -rfv lxqt/usr /
+    xbps-install -Sy $PACKAGES_LXQT
 }
+
 icejwm(){
-cp -rfv icejwm/etc /
-cp -rfv icejwm/usr /
-xbps-install -Sy $PACKAGES_ICEJWM
+    cp -rfv icejwm/etc /
+    cp -rfv icejwm/usr /
+    xbps-install -Sy $PACKAGES_ICEJWM
 }
-case "$@" in
+
+case "$1" in
     xfce ) xfce ;;
     kde ) kde ;;
     mate ) mate ;;
     niri ) niri ;;
     lxqt ) lxqt ;;
-    icejwm ) icejwm;;
+    icejwm ) icejwm ;;
     labwc ) labwc ;;
+    * )
+        echo "Error: Debes especificar un entorno válido."
+        echo "Opciones: xfce, kde, mate, niri, lxqt, icejwm, labwc."
+        exit 1
+        ;;
 esac
