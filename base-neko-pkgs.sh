21e32a095c8eb4be8424fc53afc7f87309aa33b9
JavierC <javierc@noreply.com>
2026-07-27 12:15:27 -0400

    add::::desktops:::and::::::scripts

---
+++ base-neko-pkgs.sh
@@ -0,0 +1,398 @@
+#!/bin/bash
+# ─────────────────────────────────────────────
+# Escritorio MATE
+# ─────────────────────────────────────────────
+MATE_DESKTOP="
+    discover
+    firefox
+    mate
+    mate-extra
+    mate-tweak
+    mate-polkit
+    mate-terminal
+    mpv
+    pluma
+    caja-wallpaper
+    caja-sendto
+    caja-open-terminal
+    caja-extensions
+    atril
+    gnome-screenshot
+    gnome-keyring
+    gvfs-afc
+    gvfs-mtp
+    gvfs-smb
+    lightdm
+    lightdm-webkit2-greeter
+    lightdm-gtk-greeter-settings
+    libnotify
+    numlockx
+    picom
+    lxappearance
+    discover
+"
+#XFCE DESKTOP
+XFCE2="
+    xfce4
+    xfce4-whiskermenu-plugin
+    gnome-themes-standard
+    xfce4-pulseaudio-plugin
+    xfce4-screenshooter
+    atril
+    gvfs-afc
+    gvfs-mtp
+    firefox
+    gvfs-smb
+    udisks2
+    lightdm
+    lightdm-webkit2-greeter
+    lightdm-gtk-greeter-settings
+    libnotify
+    numlockx
+"
+XFCE="
+    gnome-software
+    xfce4
+    xfce4-whiskermenu-plugin
+    gnome-themes-standard
+    xfce4-pulseaudio-plugin
+    xfce4-screenshooter
+    atril
+    gvfs-afc
+    gvfs-mtp
+    firefox
+    gvfs-smb
+    udisks2
+    lightdm
+    lightdm-webkit2-greeter
+    lightdm-gtk-greeter-settings
+    libnotify
+    numlockx
+"
+LXQT="
+    kate
+    discover
+    mpv
+    lxqt
+    xfwm4
+    xfwm4-themes
+    lightdm
+    lightdm-webkit2-greeter
+    lightdm-gtk-greeter-settings
+    gvfs-afc
+    gvfs-mtp
+    gvfs-smb
+    udisks2
+    firefox
+    qt6-virtualkeyboard
+    qt6-svg
+    qt6-multimedia
+    gum
+"
+LXDE="
+    lxde
+    lightdm
+    lightdm-gtk-greeter
+    gvfs-afc
+    gvfs-mtp
+    gvfs-smb
+    udisks2
+    mpv
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal
+    firefox
+"
+
+I3="
+    discover
+    i3
+    lightdm
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal
+    lightdm-gtk-greeter
+    polybar
+    rofi
+    kitty
+    geany
+    picom
+    qt6ct
+    lxappearance
+    feh
+    mpv
+    dex
+    polkit-gnome 
+    pulseaudio-utils
+    setxkbmap
+    brightnessctl
+    playerctl
+    maim
+    xclip
+    xdotool
+    nemo
+    dmenu
+    git
+    ark
+    curl
+    wget
+    unzip
+    cargo
+    pkg-config
+    openssl
+    libxcb
+    xcb-util
+    xcb-util-image
+    xcb-util-keysyms
+    xcb-util-renderutil
+    xcb-util-wm
+    libxkbcommon
+    font-awesome6
+    fontconfig
+    ImageMagick
+    gvfs-afc
+    gvfs-mtp
+    gvfs-smb
+    udisks2
+    firefox
+"
+
+ICEJWM="
+    discover
+    ristretto
+    xarchiver
+    arandr
+    jwm
+    jwmkit-neko
+    icewm
+    mpv
+    pcmanfm
+    alacritty
+    ristretto
+    lxappearance
+    atril
+    lightdm
+    lightdm-gtk-greeter
+    gvfs-afc
+    gvfs-mtp
+    gvfs-smb
+    udisks2
+    firefox
+    xdg-desktop-portal
+    xdg-desktop-portal-gtk
+    mate-polkit
+    xfce4-screenshooter
+"
+
+KDE="
+    kde-plasma
+    konsole
+    kate
+    firefox
+    dolphin
+    discover
+    gvfs-afc
+    gvfs-mtp
+    gvfs-smb
+    mpv
+    sddm
+    plasma-framework
+    kdeconnect
+    kdegraphics-thumbnailers
+    kde-baseapps
+    qt6-virtualkeyboard
+    qt6-svg
+    qt6-multimedia
+    gum
+    okular
+    spectacle
+    gwenview
+    ark
+"
+CINNAMON="
+    cinnamon
+    gvfs-afc
+    gnome-software
+    gvfs-mtp
+    gvfs-smb
+    lightdm
+    colord
+    gnome-terminal
+"
+
+LABWC="
+    discover
+    ristretto
+    xarchiver
+	lightdm
+	gvfs-afc
+    gvfs-mtp
+    gvfs-smb
+	wlr-randr
+	mako
+	xwayland-satellite
+	swaylock
+	mate-polkit
+	labwc
+	alacritty
+	kanshi
+	sfwbar
+	xdg-desktop-portal
+	xdg-desktop-portal-wlr
+	playerctl
+	rofi
+	nwg-look
+	gtk-update-icon-cache
+	wl-clipboard
+	wlopm
+	mpv
+	ristretto
+	geany
+	grim
+	slurp
+	gtksourceview
+	json-c
+	yad
+	waterfox
+	gtk-layer-shell
+	gtkmm
+	pcmanfm
+    wlr-randr
+    wdisplays
+"
+
+NIRI="
+    discover
+    ristretto
+    xarchiver
+	gvfs-afc
+    gvfs-mtp
+    gvfs-smb
+    wlr-randr
+    wdisplays
+	mate-polkit
+    Thunar
+    xwayland-satellite
+	emptty
+	niri
+	noctalia
+	foot
+	xdg-desktop-portal
+	xdg-desktop-portal-gnome
+	xdg-desktop-portal-wlr
+	wl-clipboard
+	mako
+	wlsunset
+	nwg-look
+	gtk-update-icon-cache
+	wl-clipboard
+	wlopm
+	mpv
+	ristretto
+	geany
+	grim
+	slurp
+	gtksourceview
+	json-c
+	yad
+	waterfox
+	gtk-layer-shell
+	gtkmm
+"
+
+# ─────────────────────────────────────────────
+# Aplicaciones de escritorio
+# ─────────────────────────────────────────────
+DESKTOP_APPS="
+    gparted
+"
+
+# ─────────────────────────────────────────────
+# Flatpak + portales
+# ─────────────────────────────────────────────
+FLATPAK="
+    flatpak
+    xdg-desktop-portal
+    xdg-desktop-portal-gtk
+    octoxbps
+"
+
+# ─────────────────────────────────────────────
+# Fuentes
+# ─────────────────────────────────────────────
+FONTS="
+    noto-fonts-emoji
+    noto-fonts-cjk
+    noto-fonts-ttf
+    font-awesome
+    dejavu-fonts-ttf
+    liberation-fonts-ttf
+    font-misc-misc
+    terminus-font
+"
+
+
+MATE=${MATE_DESKTOP}
+DEFAULT="
+    ${DESKTOP_APPS}
+    ${FLATPAK}
+    ${FONTS}
+"
+# ─────────────────────────────────────────────
+# Construir la lista completa de paquetes
+# ─────────────────────────────────────────────
+MATE_PACKAGES="
+    ${DEFAULT}
+    ${MATE}
+"
+
+KDE_PACKAGES="
+    ${DEFAULT}
+    ${KDE}
+"
+
+LXQT_PACKAGES="
+    ${DEFAULT}
+    ${LXQT}
+"
+I3_PACKAGES="
+    ${DEFAULT}
+    ${I3}
+"
+
+XFCE_PACKAGES="
+    ${DEFAULT}
+    ${XFCE}
+"
+
+ICEJWM_PACKAGES="
+    ${DEFAULT}
+    ${ICEJWM}
+"
+
+LXDE_PACKAGES="
+    ${DEFAULT}
+    ${LXDE}
+"
+
+CINNAMON_PACKAGES="
+    ${DEFAULT}
+    ${CINNAMON}
+"
+LABWC_PACKAGES="
+    ${DEFAULT}
+    ${LABWC}
+"
+NIRI_PACKAGES="
+    ${DEFAULT}
+    ${NIRI}
+"
+# Limpiar espacios extra y newlines, convertir a una línea
+PACKAGES_MATE=$(echo ${MATE_PACKAGES} | tr -s ' ')
+PACKAGES_KDE=$(echo ${KDE_PACKAGES} | tr -s ' ')
+PACKAGES_LXQT=$(echo ${LXQT_PACKAGES} | tr -s ' ')
+PACKAGES_I3=$(echo ${I3_PACKAGES} | tr -s ' ')
+PACKAGES_XFCE=$(echo ${XFCE_PACKAGES} | tr -s ' ')
+PACKAGES_ICEJWM=$(echo ${ICEJWM_PACKAGES} | tr -s ' ')
+PACKAGES_LXDE=$(echo ${LXDE_PACKAGES} | tr -s ' ')
+PACKAGES_CINNAMON=$(echo ${CINNAMON_PACKAGES} | tr -s ' ')
+PACKAGES_LABWC=$(echo ${LABWC_PACKAGES} | tr -s ' ')
+PACKAGES_NIRI=$(echo ${NIRI_PACKAGES} | tr -s ' ')
---
+++ desktop-set.sh
@@ -0,0 +1,47 @@
+#! /usr/bin/env bash
+. ./base-neko-pkgs.sh
+
+xfce(){
+cp -rfv xfce/etc /
+cp -rfv xfce/usr /
+xbps-install -Sy $PACKAGES_XFCE
+}
+niri(){
+cp -rfv niri/etc /
+cp -rfv niri/usr /
+xbps-install -Sy $PACKAGES_NIRI
+}
+kde(){
+cp -rfv kde/etc /
+cp -rfv kde/usr /
+xbps-install -Sy $PACKAGES_KDE
+}
+mate(){
+cp -rfv mate/etc /
+cp -rfv mate/usr /
+xbps-install -Sy $PACKAGES_MATE
+}
+labwc(){
+cp -rfv labwc/etc /
+cp -rfv labwc/usr /
+xbps-install -Sy $PACKAGES_LABWC
+}
+lxqt(){
+cp -rfv lxqt/etc /
+cp -rfv lxqt/usr /
+xbps-install -Sy $PACKAGES_LXQT
+}
+icejwm(){
+cp -rfv icejwm/etc /
+cp -rfv icejwm/usr /
+xbps-install -Sy $PACKAGES_ICEJWM
+}
+case "$@" in
+    xfce ) xfce ;;
+    kde ) kde ;;
+    mate ) mate ;;
+    niri ) niri ;;
+    lxqt ) lxqt ;;
+    icejwm ) icejwm;;
+    labwc ) labwc ;;
+esac
