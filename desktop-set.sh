2e7d9eb33588db8f538e4c8c53549f5d20ab3f67
javiercplus <javiercplus@noreply.codeberg.org>
2026-07-29 00:53:37 +0200

    Actualizar desktop-set.sh

--- desktop-set.sh
+++ desktop-set.sh
@@ -4,18 +4,21 @@ rm_mate(){
 xbps-remove -Ro $PACKAGES_MATE
 }
 xfce(){
+    rm_mate
     cp -rfv xfce/etc /
     cp -rfv xfce/usr /
     xbps-install -Sy $PACKAGES_XFCE
 }
 
 niri(){
+    rm_mate
     cp -rfv niri/etc /
     cp -rfv niri/usr /
     xbps-install -Sy $PACKAGES_NIRI
 }
 
 kde(){
+    rm_mate
     cp -rfv kde/etc /
     cp -rfv kde/usr /
     xbps-install -Sy $PACKAGES_KDE
@@ -28,18 +31,21 @@ mate(){
 }
 
 labwc(){
+    rm_mate
     cp -rfv labwc/etc /
     cp -rfv labwc/usr /
     xbps-install -Sy $PACKAGES_LABWC
 }
 
 lxqt(){
+    rm_mate
     cp -rfv lxqt/etc /
     cp -rfv lxqt/usr /
     xbps-install -Sy $PACKAGES_LXQT
 }
 
 icejwm(){
+    rm_mate
     cp -rfv icejwm/etc /
     cp -rfv icejwm/usr /
     xbps-install -Sy $PACKAGES_ICEJWM
