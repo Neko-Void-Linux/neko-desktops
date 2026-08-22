4a2482851ea4b9c103b62e411fee4e0d28c0397e
javiercplus <66850752+javiercplus@users.noreply.github.com>
2026-08-21 20:27:42 -0400

    noctalia::labwc

--- labwc/etc/skel/.config/labwc-themes/everforest/set.sh
+++
@@ -1,4 +0,0 @@
-cp ~/.config/labwc-themes/everforest/labwc/rc.xml ~/.config/labwc
-cp -r ~/.config/labwc-themes/everforest/sfwbar/* ~/.config/sfwbar
-labwc --reconfigure
-pkill sfwbar; sfwbar >/dev/null 2>&1 & disown
--- labwc/etc/skel/.config/labwc-themes/red/set.sh
+++
@@ -1,4 +0,0 @@
-cp ~/.config/labwc-themes/red/labwc/rc.xml ~/.config/labwc
-cp -r ~/.config/labwc-themes/red/sfwbar/* ~/.config/sfwbar
-labwc --reconfigure
-pkill sfwbar; sfwbar >/dev/null 2>&1 & disown
